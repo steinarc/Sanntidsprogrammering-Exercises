704bd0c237f921fecb51e81b2d261777dc7137e7
karineav <karineav@stud.ntnu.no>
2017-01-23 18:24:45 +0100

    Update UDP_receive.py

--- Oving_3/UDP_receive.py
+++ Oving_3/UDP_receive.py
@@ -1,1 +1,11 @@
+import socket
 
+UDP_IP = "127.0.0.1" #"109.189.14.115"
+UDP_PORT = 20016
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('',UDP_PORT))
+
+while True:
+    data, addr = sock.recvfrom(1024)
+    print ("received message:", data)
