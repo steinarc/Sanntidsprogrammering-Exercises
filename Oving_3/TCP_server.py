64a39038e04e19364017d714015f73ffc987a050
steinarc <Steinar_T_Chri@hotmail.com>
2017-01-24 13:11:56 +0100

    Add files via upload

--- Oving_3/TCP_server.py
+++ Oving_3/TCP_server.py
@@ -1,18 +1,30 @@
 import socket
-
-TCP_IP = "129.241.187.151" 
-TCP_PORT = 34933
-
+import sys
+ 
+HOST = ''   # Symbolic name, meaning all available interfaces
+PORT = 8888 # Arbitrary non-privileged port
+ 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind((TCP_IP,TCP_PORT))
-s.listen(1)
-
-conn,addr = s.accept()
-print 'Connection address:', addr
-
+print ('Socket created')
+ 
+#Bind socket to local host and port
+try:
+    s.bind((HOST, PORT))
+except socket.error as msg:
+    print ('Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1])
+    sys.exit()
+     
+print ('Socket bind complete')
+ 
+#Start listening on socket
+s.listen(10)
+print ('Socket now listening')
+ 
+#now keep talking with the client
 while 1:
-	data = conn.recv(20)
-	print ("received datra: ", data)
-	conn.send(data)
-
-conn.close()
+    #wait to accept a connection - blocking call
+    conn, addr = s.accept()
+    print ('Connected with ' + addr[0] + ':' + str(addr[1]))
+    conn.send(b'Server sier: du er en dust')
+     
+s.close()
