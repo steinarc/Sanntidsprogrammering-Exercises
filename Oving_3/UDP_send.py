1b42b6c6fa9c331034393ab6d95a1f6cfc9f55ad
karineav <karineav@stud.ntnu.no>
2017-01-27 10:44:23 +0100

    Update UDP_send.py

--- Oving_3/UDP_send.py
+++ Oving_3/UDP_send.py
@@ -1,14 +1,18 @@
 import socket
 import time
 
-UDP_IP = "127.0.0.1"
-UDP_PORT = 20016
-MESSAGE = b"Hei paa deg!"
+UDP_IP = "127.0.0.255"
+UDP_PORT = 20018
+MESSAGE_1 = "Hei paa deg!"
+MESSAGE_2 = "Det er meg!"
 
-print ("message: ", MESSAGE)
+print ("Message 1 is: ", MESSAGE_1)
+print ("Message 2 is: ", MESSAGE_2)
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
-    sock.sendto(MESSAGE, (UDP_IP, UDP_PORT))
-    time.sleep(1)
+	sock.sendto(MESSAGE_1, (UDP_IP, UDP_PORT))
+	time.sleep(1)
+	sock.sendto(MESSAGE_2, (UDP_IP, UDP_PORT))
+	time.sleep(1)
