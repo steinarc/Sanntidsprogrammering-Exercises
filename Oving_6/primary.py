cb6c2db9684c94312fa59553ef2238d7d28447a6
steinarc <Steinar_T_Chri@hotmail.com>
2017-02-17 13:55:40 +0100

    Add files via upload

--- Oving_6/primary.py
+++ Oving_6/primary.py
@@ -1,82 +1,53 @@
 from socket import *
 import time
-from multiprocessing import Process
+import select
 
-from sys import executable
-from subprocess import Popen, CREATE_NEW_CONSOLE
+from subprocess import *
 import sys
-from queue import *
-
-REMOTE_IP = "127.0.0.1"         
-PORT = 30000  
-
-backup = socket(AF_INET, SOCK_DGRAM)
-backup.bind(('',PORT))
-
-def make_backup():                   
-	Popen([sys.executable, 'primary.py'], creationflags = CREATE_NEW_CONSOLE) #shell = True
-
-#backup
-def receive_put(port):
-
-	s = socket(AF_INET, SOCK_DGRAM)
-		
-	try :
-		s.bind(('',port))
-	except (socket.error, msg):
-		print ('Failed to bind a socket. Error Code : '.encode() + str(msg[0]) + ' Message '.encode() + msg[1])
-		sys.exit()
-		
-	m = s.recvfrom(1024)
-
-	if (m != "I am alive"):
-		q = queue()
-		q.put(m)
-	#return (m[0])
+import os
 
 
-#primary should broadcast: i am alive
-def broadcast_alive(port):
-	main = socket(AF_INET, SOCK_DGRAM)
-	main.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
-	main.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-	
+def backup_does_things():
+	print("I AM BACKUP\n")
+
+	socket_receive = socket(AF_INET, SOCK_DGRAM)
+	socket_receive.bind(('',20018))
+	socket_receive.setblocking(0)
+	last_i = 0
+
 	while(True):
-		#print("Er vi her?")
-		main.sendto("I am alive".encode(),('255.255.255.255',port))
-		time.sleep(1)
+		m = "0"
+		ready = select.select([socket_receive], [], [], 2)
+		if ready[0]:		
+			m = socket_receive.recvfrom(1024)
+			#print("I am backup, I heard that %d" % (int(m[0])))
+			last_i = int(m[0])
+		if(m == "0"):
+			socket_receive.close()	
+			primary_does_things(last_i)
 
-def counter(i):
 
-	main = socket(AF_INET, SOCK_DGRAM)
+
+def primary_does_things(i):		
+
+	os.system("gnome-terminal -x python primary.py ")
+
+	socket_send = socket(AF_INET, SOCK_DGRAM)
 	while(True):
 		i += 1
-		main.sendto(str(i).encode(), ("127.0.0.1" ,30000))
-		print(i)
+		socket_send.sendto( str(i) , ("127.0.0.1" ,20018))
+		print("I am primary, I say: %d" % (i))
 		time.sleep(1)
-	
 
 
-
-def run_primary(i):
-	print("---- MASTER -----\n")
-	Popen([sys.executable, 'primary.py'], creationflags = CREATE_NEW_CONSOLE) #åpner backup
+def spawn_backup():
+	os.system("gnome-terminal -x python primary.py ")
 
 
+backup_does_things()
 
-#def run_backup():
-
-
-def main():
-	p1 = Process(target = broadcast_alive, args = (PORT,))
-	p1.start()
-
-
-	p2 = Process(target = counter, args = (0,))
-	p2.start()
+#"Popen(["gnome-terminal -x primary.py"])
 
 
 
 
-if __name__ == '__main__':
-	main()
